69ad770a4992b083f8fe8faab64ef0b7a75f1a1a
montbup <montsepuerta11@gmail.com>
2024-11-25 02:20:14 -0600

    certifications

---
+++ src/data/certifications.js
@@ -0,0 +1,69 @@
+export const certifications = [
+    {
+      id: 1,
+      title: "Scrum Fundamentals Certification",
+      description: "With my certification in Scrum Fundamentals, where I learned the basics of this agile framework. I now understand how to manage projects efficiently using key roles like the Scrum Master and Product Owner, along with tools such as backlogs and follow-up meetings.",
+      image: "/images/certifications/SCRUM.jpg"
+    },
+    {
+      id: 2,
+      title: "Spark Overview for Scala Analytics",
+      description: "In this certification, I learned the fundamentals of Apache Spark and how to use it with Scala to analyze data quickly and efficiently. I also understood how Spark improves distributed data processing.",
+      image: "/images/certifications/certification2.png"
+    },
+    {
+      id: 3,
+      title: "MapReduce and YARN",
+      description: "This certification helped me understand how MapReduce works for parallel data processing and how YARN manages resources within the Hadoop ecosystem. It gave me insights into handling large data volumes.",
+      image: "/images/certifications/certification3.png"
+    },
+    {
+        id: 4,
+        title: "Moving Data into Hadoop",
+        description: "I specialized in transferring data into the Hadoop ecosystem. I learned to use tools and techniques to ingest and prepare data for massive-scale analysis.",
+        image: "/images/certifications/certification4.png"
+      },
+      {
+        id: 5,
+        title: "Simplifying data pipelines with Apache",
+        description: "This certification allowed me to understand how to build and manage efficient data pipelines using Apache tools, simplifying processes in data workflows",
+        image: "/images/certifications/certification5.png"
+      },
+      {
+        id: 6,
+        title: "Accessing Hadoop Data Using Hive",
+        description: "I learned to use Apache Hive to perform SQL-like queries on data stored in Hadoop. This gave me the skills to manage structured data more efficiently and effectively.",
+        image: "/images/certifications/certification6.png"
+      },
+      {
+        id: 7,
+        title: "Docker Essentials",
+        description: "I earned a certification in Docker, where I learned how to create, manage, and deploy applications using containers. I can now work with Docker images and orchestrate basic services.",
+        image: "/images/certifications/certification7.png"
+      },
+      {
+        id: 8,
+        title: "Spark Fundamentals I",
+        description: "With this certification, I delved into the fundamentals of Apache Spark, understanding its architecture and how it outperforms MapReduce in speed and flexibility for analyzing large data volumes.",
+        image: "/images/certifications/certification8.png"
+      },
+      {
+        id: 9,
+        title: "Big Data 101",
+        description: "This certification gave me a complete introduction to the world of Big Data, from its definition to the key technologies like Hadoop and Spark.",
+        image: "/images/certifications/certification9.png"
+      },
+      {
+        id: 10,
+        title: "Hadoop 101",
+        description: "I learned the basic concepts of Hadoop, including its architecture and core components like HDFS and YARN, and how they are used in large-scale data projects.",
+        image: "/images/certifications/certification10.png"
+      },
+      {
+        id: 11,
+        title: "Scala 101",
+        description: "With this certification, I gained an understanding of Scala fundamentals, a functional and object-oriented programming language ideal for data analysis and distributed applications.",
+        image: "/images/certifications/certification11.png"
+      },
+  ];
+  
